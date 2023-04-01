a9261a4dfaf291d9472128a5e886c56ab8c8d2cb
dteplitz <dteplitz@prismamp.com>
2023-04-01 20:35:50 -0300

    Get drones loaded

--- src/main/java/com/example/dronesv2/controller/DroneController.java
+++ src/main/java/com/example/dronesv2/controller/DroneController.java
@@ -3,6 +3,7 @@ package com.example.dronesv2.controller;
 
 import com.example.dronesv2.dto.DroneDTO;
 import com.example.dronesv2.model.Drone;
+import com.example.dronesv2.model.DroneState;
 import com.example.dronesv2.model.Medication;
 import com.example.dronesv2.service.DroneService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,13 +60,13 @@ public class DroneController {
             return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
-/*
+
     @GetMapping("/available-for-loading")
     public ResponseEntity<?> getAvailableDronesForLoading() {
         List<Drone> availableDrones = droneService.getDronesByState(DroneState.LOADED);
         return ResponseEntity.ok(availableDrones);
     }
-
+/*
     @GetMapping("/{serialNumber}/battery-level")
     public ResponseEntity<?> getDroneBatteryLevel(@PathVariable String serialNumber) throws DroneNotFoundException {
         double batteryLevel = droneService.getDroneBatteryLevel(serialNumber);
